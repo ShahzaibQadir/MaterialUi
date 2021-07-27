8075f7af00dbef2306357c181c40bd9456b78a9a
ShahzaibQadir <77103950+ShahzaibQadir@users.noreply.github.com>
2021-07-27 14:44:49 +0500

    materialui

--- src/App.js
+++ src/App.js
@@ -1,24 +1,116 @@
-import logo from './logo.svg';
 import './App.css';
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button } from '@material-ui/core';
+import { PhotoCamera } from '@material-ui/icons';
+
+// import Button from '@material-ui/core/Button';
+// import ButtonGroup from '@material-ui/core/ButtonGroup';
+
+
+import useStyles from './style';
+
+const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <AppBar position="relative">
+        <Toolbar>
+          <PhotoCamera className={classes.icon}/>
+          <Typography variant="h6">
+            Photo Album
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <main>
+        <div className={classes.container}>
+          <Container maxWidth="sm" >
+            <Typography
+              variant="h2"
+              align="center"
+              color="textPrimary"
+              gutterBottom>
+              Photo Album
+            </Typography>
+
+
+            <Typography
+              variant="h5"
+              align="center"
+              color="textSecondary"
+              paragraph>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum unde autem quo illo ut pariatur rerum reiciendis mollitia ipsa consequuntur exercitationem nemo vitae incidunt, animi est minima aperiam. Itaque, fuga!
+            </Typography>
+
+            <div className={classes.button}>
+              <Grid container spacing={2} justify="center">
+                <Grid item >
+                      <Button variant="contained" color="primary">
+                        See my photo Primary
+                      </Button>                      
+
+                      <Button variant="outlined" color="primary">
+                        Secondary
+                      </Button>                      
+                </Grid>
+              </Grid>
+            </div>
+
+          </Container>
+        </div>
+
+        <Container className={classes.cardGrid} maxWidth="md">
+          <Grid container spacing={4}>
+            {cards.map((card)=>(
+
+            <Grid item key={card} xs={12} sm={6} md={4}>
+              <Card className={classes.card}>
+                <CardMedia
+                  className={classes.cardMedia}
+                  image="https://source.unsplash.com/random"
+                  title="Image title" 
+                />
+              <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="h5">
+                      Heading
+                  </Typography>
+                  <Typography >
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis iste nisi nulla, ex odit, sunt, exercitationem praesentium consequuntur quaerat excepturi earum incidunt officia? Eligendi quis quo obcaecati commodi, numquam ea!
+                  </Typography>
+              </CardContent>
+
+              <CardActions>
+
+                  <Button size="small" color="primary">View</Button>
+                  <Button size="small" color="primary">Edit</Button>
+
+              </CardActions>
+
+              </Card>
+            </Grid>
+            ))}
+          </Grid>
+        </Container>
+
+
+        {/* <ButtonGroup variant="contained" color="primary" aria-label="contained primary button group">
+          <Button>One</Button>
+          <Button>Two</Button>
+          <Button>Three</Button>
+        </ButtonGroup> */}
+      
+      
+      </main>
+      <footer className={classes.footer}>
+        <Typography variant="h6" align="center" gutterBottom>
+            footer
+        </Typography>
+        <Typography variant="subtitle1" align="center" color="textSecondary">
+            Something for footer
+        </Typography>
+      </footer>
+    </>
   );
 }
 
